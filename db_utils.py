79b4aea38f72f3f176b8b7aa683885d0aabe3393
bodie wood <bodie1@macbook-air.lan>
2023-05-23 14:57:04 -0700

    add function to conver pandas dataframe to a database

---
+++ db_utils.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+def dataframe_to_database(df, table_name):
+    """Convert a pandas dataframe to a database.
+        Args:
+            df (dataframe): pd.DataFrame which is to be converted to a database
+            table_name (string): Name of the table within the database
+        Returns:
+            engine: SQLAlchemy engine object
+    """
+    engine = create_engine(f'sqlite:///:memory:', echo=False)
+    df.to_sql(name=table_name, con=engine, index=False)
+    return engine
+
